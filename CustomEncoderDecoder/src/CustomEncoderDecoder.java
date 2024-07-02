d07f598541830d1313f602047995c0c511831db3
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-07-03 01:43:07 +0800

    Refactor CustomEncoderDecoder Class
    Changed implementation to a interactive CLI based application, user to input the selected option with additional relevant input. Results are output to user depending on actions performed, with error handled.

--- CustomEncoderDecoder/src/CustomEncoderDecoder.java
+++ CustomEncoderDecoder/src/CustomEncoderDecoder.java
@@ -1,36 +1,58 @@
+import java.util.Scanner;
 
 public class CustomEncoderDecoder {
 	public static void main(String[] args) {
 		EncoderDecoder customEncoderDecoder = new EncoderDecoder();
-		customEncoderDecoder.setReferenceTable("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()*+,-./");
+        Scanner scanner = new Scanner(System.in);
+        String referenceTable;
+        char offsetChar;
+        String plainText;
 		String encodedText;
 		String decodedText;
-		
-		/*
-		 * Example 1.
-		 * Plain text: HELLO WORLD
-		 * Offset Character: B
-		 */
-		System.out.println("-------- EXAMPLE 1 --------\nPlain text: \tHELLO WORLD\nOffset Character: \tB\n");
-		customEncoderDecoder.setOffsetChar('B');
-        encodedText = customEncoderDecoder.encode("HELLO WORLD");
-        System.out.println("Encoded Text: \t" + encodedText);
         
-        decodedText = customEncoderDecoder.decode(encodedText);
-        System.out.println("Decoded Text: \t" + decodedText);
-        
-        /*
-		 * Example 2.
-		 * Plain text: HELLO WORLD
-		 * Offset Character: F
-		 */
-		System.out.println("\n\n-------- EXAMPLE 2 --------\nPlain text: \tHELLO WORLD\nOffset Character: \tF\n");
-		customEncoderDecoder.setOffsetChar('F');
-        encodedText = customEncoderDecoder.encode("HELLO WORLD");
-        System.out.println("Encoded Text: \t" + encodedText);
-        
-        decodedText = customEncoderDecoder.decode(encodedText);
-        System.out.println("Decoded Text: \t" + decodedText);
+        System.out.println("Welcome to the Custom Encoder Decoder application!");
+        System.out.println("\nSelect Options: \n- 1: Configure reference table \n- 2: Configure offset character \n- 3: Encode plain text \n- 4: Decode plain text \n- 5: Exit application");
+
+        app: while (true) {
+            try {
+                System.out.print("\nEnter option: ");
+                String option = scanner.nextLine();
+                
+                switch(option) {
+                	case "1":
+                		System.out.print("Enter reference table string: ");
+                		referenceTable = scanner.nextLine();
+                        customEncoderDecoder.setReferenceTable(referenceTable);
+                        break;
+                	case "2":
+                		System.out.print("Enter offset character: ");
+                        offsetChar = scanner.nextLine().charAt(0);
+                        customEncoderDecoder.setOffsetChar(offsetChar);
+                        break;
+                	case "3":
+                		System.out.print("Enter plain text to encode: ");
+                		plainText = scanner.nextLine();
+                		encodedText = customEncoderDecoder.encode(plainText);
+                        System.out.println("Encoded Text: " + encodedText);
+                        break;
+                	case "4":
+                		System.out.print("Enter encoded text to decode: ");
+                		encodedText = scanner.nextLine();
+                		decodedText = customEncoderDecoder.decode(encodedText);
+                        System.out.println("Decoded Text: " + decodedText);
+                        break;
+                	case "5":
+                		break app;
+            		default:
+            			System.out.println("Invalid option given! Please select one of the following options: \n- 1: Configure reference table \n- 2: Configure offset character \n- 3: Encode plain text \n- 4: Decode plain text \n- 5: Exit application");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+        scanner.close();
+        System.out.println("Goodbye!");
 	}
 
 }
